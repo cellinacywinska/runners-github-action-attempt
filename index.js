abf0af994293de92b6e89ae90ef09b0c1bc57aba
Celina Cywinska <celinacywinska@gmail.com>
2022-07-19 11:30:53 -0700

    testing

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const configPath = core.getInput('pulumi-config-path');
+  const pulumiGoal = core.getInput('pulumi-goal');
+  const stackName = core.getInput('pulumi-stack-name');
+  const cloudProvider = core.getInput('pulumi-cloud-provider');
+  const cloudArch = core.getInput('cloud-architecture');
+
+  console.log(`Path: ${configPath} ${pulumiGoal} ${stackName} ${cloudProvider} ${cloudArch}`);
+
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
+
+// output is a placeholder 
+// define enumerated values 
+// load the inputs
+// validate pulumi goal and arch
+// against the pulumi goal, then cloud arch
+// all inputs validated --> then check if arm64 and gcp then error 
+
+
+// branch 
+// tags are copies of branches that are frozen
